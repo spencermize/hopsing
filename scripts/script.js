29de2e2d0b5e90776cfd3ef18da005caf4227bb7
spencermize <spence.bassman@gmail.com>
2015-01-28 23:38:21 -0500

    i can do modals

--- scripts/script.js
+++ scripts/script.js
@@ -6,24 +6,42 @@ $app.config(function($interpolateProvider) {
 $app.controller('TypeaheadCtrl', function($scope, $http) {
   $scope.asyncSelected = undefined;  
   $scope.getKegs = function(val) {
-	return $http.get('http://localhost/api/kegs', {
+	return $http.get('/api/kegs', {
 
 		}).then(function(response){
 		return response.data.results;
     });
   }
 })
-$app.controller('KegCreate',function($scope,$http) {
+$app.controller('KegCreate',function($scope,$http,$modal) {
 	$scope.postKegs = function(val){
 		return $http({
 			method: 'post',
 			url: '/api/kegs',
 			data: "name="+$scope.newKeg.name+"&abv="+$scope.newKeg.abv+"&type=" +$scope.newKeg.type,
 			headers: {'Content-Type' : 'application/x-www-form-urlencoded'}
-		}).then(function(response){
-			
+		}).success(function(response){
+			$scope.response = response;
+			$scope.open();
 		});
-		
 	}
-	
+  $scope.open = function () {
+	  
+    var modalInstance = $modal.open({
+      templateUrl: 'ModalContent.html',
+      controller: 'ModalMsgInstance',
+      resolve: {
+        response: function () {
+          return $scope.response;
+        }
+      }
+    });
+  };	
 });
+$app.controller('ModalMsgInstance',function($scope,$modalInstance,response){
+	$scope.title = "Success";
+	$scope.msg = response.msg;
+	$scope.ok = function () {
+		$modalInstance.close();
+	};
+})
